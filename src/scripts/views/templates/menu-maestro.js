8670da7710161cda468a1a95de864ca1eb4830f0
maulanafikriash <maulanx835@gmail.com>
2023-10-28 20:41:02 +0700

    change innerHTML structure

--- src/scripts/views/templates/menu-maestro.js
+++ src/scripts/views/templates/menu-maestro.js
@@ -8,18 +8,18 @@ class MenuMaestro extends HTMLElement {
     this.innerHTML = /* html */`
       <div class="container">
         <h1 class="brand" tabindex="0">Menu Maestro</h1>
+        <button id="hamburger-button" tabindex="0" class="mobile-menu-toggle">
+          <div class="bar"></div>
+          <div class="bar"></div>
+          <div class="bar"></div>
+        </button>
         <nav id="menu-drawer" class="menu">
           <ul>
             <li><a href="#/daftar-restoran" tabindex="0">Home</a></li>
             <li><a href="#/favorite" tabindex="0">Favorite</a></li>
             <li><a href="#about-us" tabindex="0">AboutUs</a></li>
           </ul>
-        </nav>
-        <button id="hamburger-button" class="mobile-menu-toggle">
-          <div class="bar"></div>
-          <div class="bar"></div>
-          <div class="bar"></div>
-        </button>
+        </nav>   
       </div>
     `;
   }
