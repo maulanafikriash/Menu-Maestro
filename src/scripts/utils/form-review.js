f1f2aa94b0cd63b746a2d35aeb15b9ec6af3890a
maulanafikriash <maulanx835@gmail.com>
2023-10-30 12:53:24 +0700

    change DOM

--- src/scripts/utils/form-review.js
+++ src/scripts/utils/form-review.js
@@ -11,14 +11,23 @@ const Reviews = (url, name, review) => {
   const container = document.querySelector('#post-review');
   const times = { year: 'numeric', month: 'long', day: 'numeric' };
   const date = new Date().toLocaleDateString('id-ID', times);
-  const addReview = /* html */`                   
-    <div class="customer-review__container">
-        <h4 tabindex="0">${name}</h4>
-        <p tabindex="0">${date}</p>
-        <p tabindex="0">${review}</p>
-    </div>
-      `;
-  container.innerHTML += addReview;
+
+  const reviewContainer = document.createElement('div');
+  reviewContainer.classList.add('customer-review__container');
+
+  const nameElement = document.createElement('h4');
+  nameElement.textContent = name;
+  reviewContainer.appendChild(nameElement);
+
+  const dateElement = document.createElement('p');
+  dateElement.textContent = date;
+  reviewContainer.appendChild(dateElement);
+
+  const reviewElement = document.createElement('p');
+  reviewElement.textContent = review;
+  reviewContainer.appendChild(reviewElement);
+
+  container.appendChild(reviewContainer);
 };
 
 export default Reviews;
