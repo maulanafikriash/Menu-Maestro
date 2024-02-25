05e8f9afa0713ea7d73f3c2167990c236f90485f
maulanafikriash <maulanx835@gmail.com>
2024-02-26 06:17:39 +0700

    fix: change link medsos

--- src/scripts/views/templates/about-us.js
+++ src/scripts/views/templates/about-us.js
@@ -11,8 +11,8 @@ class AboutUsSection extends HTMLElement {
       <p tabindex="0">Follow me on social media </p>
     <div class="icons">
       <a href="https://github.com/maulanafikriash" target="_blank"><i class="fa-brands fa-github"></i></a>
-      <a href="https://www.instagram.com/maulanafikri.ash/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
-      <a href="https://www.linkedin.com/in/maulana-fikri-ash-shidiq-63749227b/" target="_blank"><i
+      <a href="https://www.instagram.com/maulanafikriash/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+      <a href="https://www.linkedin.com/in/maulanafikriash/" target="_blank"><i
           class="fa-brands fa-linkedin"></i></a>
     </div>
     </section>
